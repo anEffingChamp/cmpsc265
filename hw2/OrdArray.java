334d96d277b655d4f778fa1edd02096eeb9b4c34
Jonathan Smalls <jon@smalls.rocks>
2019-09-13 20:53:32 -0400

    feature: main() to run function, new sorting logic

--- hw2/OrdArray.java
+++ hw2/OrdArray.java
@@ -71,7 +71,7 @@ public void display() {             // displays array contents
  * Implement the merge()method to the OrdArray class so that you can merge two
  * ordered arrays into an ordered destination array and return that array
  */
-public long[] merge(OrdArray arr)
+public void merge(OrdArray arr)
 {
     long[] input     = arr.a;
     int outputLength = nElems + arr.size();
@@ -81,15 +81,30 @@ public long[] merge(OrdArray arr)
     loop < outputLength;
     loop++
     ) {
-        if (a[inputIndex[0]] < input[inputIndex[1]]) {
-            output[loop] = a[inputIndex[0]];
-            inputIndex[0]++;
-        } else {
-            output[loop] = input[inputIndex[1]];
-            inputIndex[1]++;
+        if (inputIndex[0] + inputIndex[1] < outputLength) {
+            if (inputIndex[0] < nElems
+            &&  a[inputIndex[0]] < input[inputIndex[1]]
+            ) {
+                output[loop] = a[inputIndex[0]];
+                if (inputIndex[0] < nElems) {
+                    inputIndex[0]++;
+                }
+            } else if (inputIndex[1] < arr.size()) {
+                output[loop] = input[inputIndex[1]];
+                if (inputIndex[1] < arr.size()) {
+                    inputIndex[1]++;
+                }
+            }
         }
     }
-    return output;
+    for (int loop = 0;
+    loop < outputLength;
+    loop++
+    ) {
+        System.out.print(output[loop] + " ");  // display it
+    }
+    System.out.println("");
+    return;
 }
 /*
  * Write code in the main()method to create two ordered arrays with arbitrary
@@ -98,6 +113,21 @@ public long[] merge(OrdArray arr)
  */
 public static void main(String[] args)
 {
- // YOUR CODES
+    int count          = 10;
+    OrdArray firstOrd  = new OrdArray(count);
+    OrdArray secondOrd = new OrdArray(count);
+    for (int loop = 0;
+    loop < count;
+    loop++
+    ) {
+        firstOrd.insert(loop);
+        secondOrd.insert(loop * 2);
+    }
+    System.out.println("The first ordered array is:");
+    firstOrd.display();
+    System.out.println("The second ordered array is:");
+    secondOrd.display();
+    System.out.println("The merged ordered array is:");
+    firstOrd.merge(secondOrd);
 }
 }  // end class OrdArray
