c2d727e5ac3c8750ec6cc390596dd84993d9c187
Jonathan Smalls <jon@smalls.rocks>
2019-09-22 19:10:42 -0400

    style: some white space updates to StackX{}

--- hw3/StackX.java
+++ hw3/StackX.java
@@ -1,10 +1,9 @@
 class StackX
-   {
-   private int maxSize;        // size of stack array
-   private long[] stackArray;
-   private int top;            // top of stack
+{
+private int maxSize;        // size of stack array
+private long[] stackArray;
+private int top;            // top of stack
 //--------------------------------------------------------------
-   
    public StackX(int s)         // constructor
       {
       maxSize = s;             // set array size
@@ -12,38 +11,35 @@ class StackX
       top = -1;                // no items yet
       }
 //--------------------------------------------------------------
-   
-   public void push(long j)    // put item on top of stack
-      {
-     // YOUR CODES
-      }
+
+public void push(long j)    // put item on top of stack
+{
+}
 //--------------------------------------------------------------
-   
-   public long pop()           // take item from top of stack
-      {
-      // YOUR CODES
-     return -1; // This is for compilation. You need to change it.
-      }
+public long pop()           // take item from top of stack
+{
+return -1; // This is for compilation. You need to change it.
+}
 //--------------------------------------------------------------
-   
+
    public long peek()          // peek at top of stack
       {
       return stackArray[top];
       }
 //--------------------------------------------------------------
-   
+
    public boolean isEmpty()    // true if stack is empty
       {
       return (top == -1);
       }
 //--------------------------------------------------------------
-   
+
    public boolean isFull()     // true if stack is full
       {
       return (top == maxSize-1);
       }
 //--------------------------------------------------------------
-   
+
    public void display() {
      for (int i=top; i>=0; i--){
        System.out.print(stackArray[i]+" ");
@@ -52,7 +48,7 @@ class StackX
    }
 
 //----------------------------------------------------------------
-   
+
     public static void main(String[] args)
     {
       StackX theStack = new StackX(5);  // make new stack
@@ -63,17 +59,17 @@ class StackX
       theStack.push(100);
       theStack.push(120);
       theStack.push(150);
-      
+
       theStack.display();
-      
+
       theStack.pop();               // pop out items
       theStack.pop();               // pop out items
       theStack.pop();               // pop out items
       theStack.pop();               // pop out items
       theStack.pop();               // pop out items
       theStack.pop();               // pop out items
-    
+
       theStack.display();
-      
+
     }  // end main()
 }  // end class StackX
