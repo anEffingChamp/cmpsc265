197acf3923f900995a96d03320d509556b3a6ee1
Jonathan Smalls <jon@smalls.rocks>
2019-09-22 21:07:25 -0400

    feature: completed RemoveDuplicate{}

--- hw3/RemoveDuplicate.java
+++ hw3/RemoveDuplicate.java
@@ -31,25 +31,45 @@ import java.util.*;
  */
 class RemoveDuplicate
 {
-public static int[] loopArray(int[] array, int loopStart, int loopEnd)
+private static String loopArray(String input, int loopStart, int loopEnd)
 {
-    int[] output = array;
+    String output = "";
     for (int loop  = loopStart;
     loop          <= loopEnd;
     loop          += 2
     ) {
-        if (loop == output.length - 1) {
+        if (loop == input.length() - 1) {
             break;
         }
-        if (output[loop] > output[loop + 1]) {
-            int valueTemporary = output[loop];
-            output[loop]       = output[loop + 1];
-            output[loop + 1]   = valueTemporary;
+        if (input.charAt(loop) != input.charAt(loop + 1)) {
+            output += input.charAt(loop);
+            output += input.charAt(loop + 1);
         }
     }
     return output;
 }
 public static void main(String[] args)
 {
+    Scanner userInput = new Scanner(System.in);
+    System.out.print("Please enter a string: ");
+    String output = RemoveDuplicate.remove(userInput.next());
+    System.out.println(
+        "After removing all adjacent characters, the string is " + output
+    );
+}
+public static String remove(String input)
+{
+    String output = input;
+    output        = RemoveDuplicate.loopArray(output, 0, output.length() - 2);
+    output        = RemoveDuplicate.loopArray(output, 1, output.length() - 1);
+    for (int loop = 0;
+    loop < output.length() - 1;
+    loop++
+    ) {
+        if (output.charAt(loop) == output.charAt(loop + 1)) {
+            output = RemoveDuplicate.remove(output);
+        }
+    }
+    return output;
 }
 }
