50f5e8bef0e741483855cc5ff23a81c5b4a976a4
Jonathan Smalls <jon@smalls.rocks>
2019-09-29 09:31:12 -0400

    documentation: problem description for MovingAverage{}

--- hw4/MovingAverage.java
+++ hw4/MovingAverage.java
@@ -1,16 +1,29 @@
-class MovingAverage {
-
-    /** Initialize your data structure here. */
-    public MovingAverage(int size) {
-        // YOUR CODES
-    }
-    
-    public double next(int val) {
-        // YOUR CODES
-      return 0;  // For compilation. you need to change it.
-    }
-    
-    public static void main(String[] args){
-      // YOUR CODES
-    }
+/*
+ * Write a class that can implement the following: Given a stream of integers
+ * and a window size, calculate the moving average of all integersin the sliding
+ * window.MovingAverage m = new MovingAverage(3);m.next(1) = 1m.next(10) = (1 +
+ * 10) / 2m.next(3) = (1 + 10 + 3) / 3m.next(5) = (10 + 3 + 5) / 3Outputs:Your
+ * outputs should look something as follows.$ java MovingAveragePlease enter a
+ * number to specify the size of the window: 3Please enter a number to be added
+ * into the window: 1The average after adding the above number is: 1.0Please
+ * enter a number to be added into the window: 10The average after adding the
+ * above number is: 5.5Please enter a number to be added into the window: 3The
+ * average after adding the above number is: 6.0
+ */
+class MovingAverage
+{
+/** Initialize your data structure here. */
+public MovingAverage(int size)
+{
+    // YOUR CODES
+}
+public double next(int val)
+{
+    // YOUR CODES
+  return 0;  // For compilation. you need to change it.
+}
+public static void main(String[] args)
+{
+  // YOUR CODES
+}
 }
