fcfb6417d787e1c5a3c24fa06b8967fa8b8f5054
Jonathan Smalls <jon@smalls.rocks>
2019-09-29 08:39:21 -0400

    documentation: description for Josephus{}

--- hw4/Josephus.java
+++ hw4/Josephus.java
@@ -1,11 +1,22 @@
 import java.util.*;
-
-class Josephus {
-  public void getJosephus(int N, int M){
-    // YOUR CODES
-  }
-  
-  public static void main(String[] args) throws Exception {
+/**
+ * In the Josephus problem from antiquity, N people are in dire straits and
+ * agree to the following strategy to reduce the population. They arrange
+ * themselves in a circle (at positions numbered from 0 to N - 1) and proceed
+ * around the circle, eliminating every Mth person until only one person is
+ * left. Legend has it that Josephus figured out where to sit to avoid being
+ * eliminated. Finish theQueue client Josephus.java that takes N and M from
+ * keyboardand prints out the order in which people are eliminated (and thus
+ * would show Josephus where to sit in the circle).
+ */
+class Josephus
+{
+public void getJosephus(int N, int M)
+{
+// YOUR CODES
+}
+public static void main(String[] args) throws Exception
+{
     Josephus josephus = new Josephus();
     Scanner input = new Scanner(System.in);
     System.out.print("Please enter a number for N indicating the total number of people: ");
@@ -13,5 +24,5 @@ class Josephus {
     System.out.print("Please enter a number for M indicating the Mth person to be eliminated: ");
     int M = input.nextInt();
     josephus.getJosephus(N, M);
-  }
+}
 }
