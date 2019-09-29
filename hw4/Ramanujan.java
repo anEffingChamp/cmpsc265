614ad65d16cad2f7c5e71f5dd218c2f19cb9cd01
Jonathan Smalls <jon@smalls.rocks>
2019-09-29 13:09:00 -0400

    feature: completed Ramanujan{}

--- hw4/Ramanujan.java
+++ hw4/Ramanujan.java
@@ -19,28 +19,49 @@ public class Ramanujan
 // and the sum of their cubes, ie, i^3 + j^3.
 private static class Pair implements Comparable<Pair>
 {
-    private int i;          // first element of the pair
-    private int j;          // second element of the pair
-    private int sumOfCubes; // i^3 + j^3
-
-    // Construct a pair (i, j).
-    Pair(int i, int j) {
-        this.i = i;
-        this.j = j;
-        sumOfCubes = i * i * i + j * j * j;
-    }
-
-    // Compare this pair to the other by sumOfCubes.
-    public int compareTo(Pair other) {
-        return sumOfCubes - other.sumOfCubes;
-    }
+private int i;          // first element of the pair
+private int j;          // second element of the pair
+private int sumOfCubes; // i^3 + j^3
+// Construct a pair (i, j).
+Pair(int i, int j)
+{
+    this.i = i;
+    this.j = j;
+    sumOfCubes = i * i * i + j * j * j;
+}
+// Compare this pair to the other by sumOfCubes.
+public int compareTo(Pair other)
+{
+    return sumOfCubes - other.sumOfCubes;
+}
 }
 public static void main(String[] args) throws Exception
 {
-    PriorityQueue<String> queue = new PriorityQueue<String>();
     System.out.print("Please enter a number: ");
-    Scanner userInput           = new Scanner(System.in);
-    int userInputValue          = userInput.nextInt();
-    System.out.println(userInputValue);
+    PriorityQueue<Pair> queue = new PriorityQueue<Pair>();
+    Scanner userInput         = new Scanner(System.in);
+    int userInputValue        = userInput.nextInt();
+    int userInputRoot         = (int) Math.cbrt(userInputValue);
+    for (int loop = 1;
+    loop < userInputRoot;
+    loop++
+    ) {
+        queue.add(new Pair(loop, loop + 1));
+    }
+    while (1 < queue.size()) {
+        Pair control = queue.remove();
+        Pair other   = queue.peek();
+        if (0              == control.compareTo(other)
+        &&  userInputValue >= control.sumOfCubes
+        ) {
+            System.out.println(
+                control.sumOfCubes + " = " + control.i + "^3 + " + control.j
+                + "^3 = " + other.i + "^3 + " + other.j + "^3"
+            );
+        }
+        if (userInputRoot > control.j) {
+            queue.add(new Pair(control.i, control.j + 1));
+        }
+    }
 }
 }
