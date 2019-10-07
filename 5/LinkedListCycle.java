eab7dd418196f461d3e298943c1d53deb5e89357
Jonathan Smalls <jon@smalls.rocks>
2019-10-06 21:05:00 -0400

    documentation: added problem description to LinkedListCycle{}

--- 5/LinkedListCycle.java
+++ 5/LinkedListCycle.java
@@ -1,43 +1,41 @@
 import java.util.Scanner;
-
+/**
+ * Description:Given a singly linked list, write a program to determine whether
+ * it has a cycle.For example, the following linked list has a cycle.Hint:you
+ * can introduce two pointers traversing the linked list from head with
+ * different speeds(like two people running on a loop with different speed).  If
+ * there is a cycle, the two pointers will finally meet with each other (the
+ * person runs faster will catch up the one who runs slower).   Otherwise, there
+ * would be no cycle.
+ */
 public class LinkedListCycle {
-  private static class Link
-  {
+    private static class Link
+    {
     public int iData;              // data item
     public Link next;              // next link in list
-    
-//-------------------------------------------------------------
     public Link(int id) // constructor
     {
-      iData = id;                 // initialize data     
+      iData = id;                 // initialize data
       next =  null;
-    }                      
-    
-//-------------------------------------------------------------
-    
+    }
     public void displayLink()      // display ourself
     {
       System.out.print("{" + iData + "} ");
     }
-  }  // end class Link
+    }  // end class Link
 ////////////////////////////////////////////////////////////////
-  
-  private static class LinkList
-  {
+
+    private static class LinkList
+    {
     private Link first;            // ref to first link on list
-    
-//-------------------------------------------------------------
     public LinkList()              // constructor
     {
       first = null;               // no links on list yet
     }
-    
-// -------------------------------------------------------------
     public boolean isEmpty()       // true if list is empty
     {
       return (first==null);
     }
-//-------------------------------------------------------------
     // insert at start of list
     public void insertFirst(int id)
     {                           // make new link
@@ -45,16 +43,14 @@ public class LinkedListCycle {
       newLink.next = first;       // newLink --> old first
       first = newLink;            // first --> newLink
     }
-//-------------------------------------------------------------
     public Link deleteFirst()      // delete first item
     {                           // (assumes list not empty)
       Link temp = first;          // save reference to link
       first = first.next;         // delete it: first-->old next
       return temp;                // return deleted link
     }
-//-------------------------------------------------------------
     public void displayList()
-   {
+    {
       System.out.print("List (first-->last): ");
       Link current = first;       // start at beginning of list
       while(current != null)      // until end of list,
@@ -64,31 +60,27 @@ public class LinkedListCycle {
       }
       System.out.println("");
     }
-//-------------------------------------------------------------
-    
     public boolean containCycle(){
       // YOUR CODES
       return true;  // For compilation.  You need to change it
     }
-    
-  }  // end class LinkList
+    }  // end class LinkList
 ////////////////////////////////////////////////////////////////
-  
-  public static void main(String[] args)
-  {
+public static void main(String[] args)
+{
     Link first = new Link(3);
     Link second = new Link(2);
     Link third = new Link(0);
     Link fourth = new Link(-4);
-    
+
     first.next = second;
     second.next = third;
     third.next = fourth;
     fourth.next = second;
-    
+
     LinkList theList = new LinkList();    // create the linked list
     theList.first = first;
-    
+
     System.out.println("Does the linked list contain a cyle?: " + theList.containCycle());
-  }  // end main()
+}  // end main()
 }  // end class LinkedListCycle
