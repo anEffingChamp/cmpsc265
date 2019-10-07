fdaf5149180d0758bb37401fa7e71c9a153fd802
Jonathan Smalls <jon@smalls.rocks>
2019-10-06 22:48:47 -0400

    documentation: added problem description for Deque{}

---
+++ 5/Deque.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * DescriptionDeque is an abstract data type defined by the following structure
+ * and operations.  A dequeis structured as a collection of data items where
+ * data items can be added and removed from either end, either front or
+ * rear.Please write a Java class for Deque of Integers, using doubly linked
+ * list, so that the following method can be
+ * implemented:MethodsDescriptionDeque()Constructor. A new deque will be
+ * instantiated that is empty.addFront(int)Add a new integerto the front of the
+ * deque;Returns nothingaddRear(int)Add a new integer to the rear of the
+ * deque;Returns nothingremoveFront()Remove and return the integer at the front
+ * from the dequeremoveRear()Remove and return the integer at the end from the
+ * deque.isEmpty()Tests to see whether the deque is emptyReturns a
+ * booleandisplay()Show all the integers currently in the deque from front to
+ * rearsize()Returns the number of integers in the dequeReturns an integer.
+ */
+public class Deque
+{
+public static void main(String[] args)
+{
+}
+}
