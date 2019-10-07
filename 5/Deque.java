4a335f1a5f8b2f65a1616df5e0c6da8cbf62a2de
Jonathan Smalls <jon@smalls.rocks>
2019-10-06 23:05:05 -0400

    style: indented function methods one level lower

--- 5/Deque.java
+++ 5/Deque.java
@@ -39,54 +39,54 @@ public class Deque
     }
     }  // end class Link
 ////////////////////////////////////////////////////////////////
-    private Link first;            // ref to first link on list
-    /*
-     * Deque()Constructor. A new deque will be
-     * instantiated that is empty.
-     */
-    public Deque()              // constructor
+private Link first;            // ref to first link on list
+/*
+ * Deque()Constructor. A new deque will be
+ * instantiated that is empty.
+ */
+public Deque()              // constructor
+{
+    first = null;               // no links on list yet
+}
+public boolean isEmpty()       // true if list is empty
+{
+  return (first==null);
+}
+/*
+ * addFront(int)Add a new integerto the front of the
+ * deque;Returns nothing
+ */
+public void addFront(int id)
+{                           // make new link
+  Link newLink = new Link(id);
+  newLink.next = first;       // newLink --> old first
+  first        = newLink;            // first --> newLink
+}
+/*
+ * removeFront()Remove and return the integer at the front
+ * from the deque
+ */
+public Link removeFront()      // delete first item
+{                           // (assumes list not empty)
+    Link temp = first;          // save reference to link
+    first     = first.next;         // delete it: first-->old next
+    return temp;                // return deleted link
+}
+/*
+ * display()Show all the integers currently in the deque from front to
+ * rear
+ */
+public void display()
+{
+    System.out.print("List (first-->last): ");
+    Link current = first;       // start at beginning of list
+    while(current != null)      // until end of list,
     {
-        first = null;               // no links on list yet
+        current.displayLink();   // print data
+        current = current.next;  // move to next link
     }
-    public boolean isEmpty()       // true if list is empty
-    {
-      return (first==null);
-    }
-    /*
-     * addFront(int)Add a new integerto the front of the
-     * deque;Returns nothing
-     */
-    public void addFront(int id)
-    {                           // make new link
-      Link newLink = new Link(id);
-      newLink.next = first;       // newLink --> old first
-      first        = newLink;            // first --> newLink
-    }
-    /*
-     * removeFront()Remove and return the integer at the front
-     * from the deque
-     */
-    public Link removeFront()      // delete first item
-    {                           // (assumes list not empty)
-        Link temp = first;          // save reference to link
-        first     = first.next;         // delete it: first-->old next
-        return temp;                // return deleted link
-    }
-    /*
-     * display()Show all the integers currently in the deque from front to
-     * rear
-     */
-    public void display()
-    {
-        System.out.print("List (first-->last): ");
-        Link current = first;       // start at beginning of list
-        while(current != null)      // until end of list,
-        {
-            current.displayLink();   // print data
-            current = current.next;  // move to next link
-        }
-        System.out.println("");
-    }
+    System.out.println("");
+}
 public static void main(String[] args)
 {
 }
