b367ab67ac7761b26f58ed035ab946004027a897
Jonathan Smalls <jon@smalls.rocks>
2019-10-11 23:03:48 -0400

    feature: correctly moved last link to front of list

--- 6/ReverseLinkedList.java
+++ 6/ReverseLinkedList.java
@@ -55,8 +55,86 @@ public class ReverseLinkedList {
       }
       System.out.println("");
     }
-    public void reverse(){
-      // YOUR CODES
+    /**
+     * reverse() reorders the linked list such that it will then run back to
+     * front. The last item will become the first, following subsequent links
+     * until the first link becomes the last.
+     */
+    public void reverse()
+    {
+        /*
+         * We can exit early if the list is empty. There is no reason to go any
+         * further.
+         */
+        if (null == this.first) {
+            System.out.println("The list is empty.");
+            return;
+        }
+        /*
+         * We can also just show the list if it has a single member. It is
+         * already sorted, so there is no reason to continue.
+         */
+        if (null == this.first.next) {
+            this.displayList();
+            return;
+        }
+        Link firstLink   = this.first;
+        Link currentLink = this.first;
+        while (null != currentLink.next.next) {
+            currentLink = currentLink.next;
+        }
+        currentLink.next.next = this.first;
+        this.first            = currentLink.next;
+        currentLink.next      = null;
+        /*
+        int linkIndex  = 0;
+        int linkTotal  = 2;
+        while (null != firstLink.next) {
+            linkTotal = this.reverseLinks(linkIndex, linkTotal);
+            linkIndex++;
+        }
+        */
+    }
+    /**
+     *
+     */
+    private int reverseLinks(int input, int linkTotal)
+    {
+        Link currentLink = this.first;
+        int linkIndex    = 0;
+        int output       = linkTotal;
+        Link targetLink  = null;
+            System.out.println(currentLink.iData);
+        while (null != currentLink.next.next) {
+            /*
+             * We need to discover the total number of links in the linked list.
+             * This will help us to keep our position as we resort the list. We
+             * initialize the value at 2, so we should only trigger this
+             * condition on the first run.
+             */
+            if (2 == linkTotal) {
+                output++;
+            }
+            if (linkIndex == input) {
+                targetLink = currentLink;
+            }
+            currentLink = currentLink.next;
+            linkIndex++;
+        }
+        /*
+         * Now we can move the last link on the list to the position after the
+         * target link. The target link is already sorted, so we use it as our
+         * starting position.
+         */
+        if (0 == input) {
+            System.out.println(currentLink.iData);
+            currentLink.next = this.first;
+            this.first       = currentLink;
+            return output;
+        }
+        targetLink.next  = currentLink.next.next;
+        currentLink.next = null;
+        return output;
     }
     }  // end class LinkList
 ////////////////////////////////////////////////////////////////
