f5d5529b90e7ffef646abe04e3ddfff35f890c4e
Jonathan Smalls <jon@smalls.rocks>
2019-10-11 20:35:41 -0400

    chore: added assignment 6

---
+++ 6/StringConstruction.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class StringConstruction {
+  public static ArrayList<String> getStrings(char[] chars, int k){
+    // YOUR CODES
+    return null;  // For compilation. You need to change this.
+  }
+  
+  public static void main(String[] args){
+    char[] chars = {'a', 'b'};
+    int k=1;
+    // YOUR CODES TO CALL THE getString() methods and
+    // display the results.
+  }
+}
