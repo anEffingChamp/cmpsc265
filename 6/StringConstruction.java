cc9932e7a8412091cd31c7ac24d65a988ca7ad9f
Jonathan Smalls <jon@smalls.rocks>
2019-10-12 08:46:06 -0400

    documentation: added problem description to StringConstruction{}

--- 6/StringConstruction.java
+++ 6/StringConstruction.java
@@ -1,15 +1,21 @@
 import java.util.*;
-
-public class StringConstruction {
-  public static ArrayList<String> getStrings(char[] chars, int k){
+/**
+ * Description:Given a set of characters and a positive integer k, print all
+ * possible strings of length k that can be formed from the given set.Please
+ * implement it using recursion.
+ */
+public class StringConstruction
+{
+public static ArrayList<String> getStrings(char[] chars, int k)
+{
     // YOUR CODES
     return null;  // For compilation. You need to change this.
-  }
-  
-  public static void main(String[] args){
+}
+public static void main(String[] args)
+{
     char[] chars = {'a', 'b'};
     int k=1;
     // YOUR CODES TO CALL THE getString() methods and
     // display the results.
-  }
+}
 }
