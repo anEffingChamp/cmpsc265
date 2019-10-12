5eccd62bf8f293619458e6f3df4853188bd9377e
Jonathan Smalls <jon@smalls.rocks>
2019-10-12 09:04:22 -0400

    feature: building character list from user input in StringConstruction{}

--- 6/StringConstruction.java
+++ 6/StringConstruction.java
@@ -13,8 +13,29 @@ public static ArrayList<String> getStrings(char[] chars, int k)
 }
 public static void main(String[] args)
 {
-    char[] chars = {'a', 'b'};
-    int k=1;
+    /*
+     * We ask the user how long the set of characters should be. Then we ask
+     * them to provide the characters in that set.
+     */
+    Scanner userInput  = new Scanner(System.in);
+    System.out.print("How many characters would you like to scramble? ");
+    int characterCount = userInput.nextInt();
+    char[] characters  = new char[characterCount];
+    System.out.println(
+        "Please enter a string at least " + characterCount
+        + " characters long: "
+    );
+    /*
+     * Now we can build the set from the provided string, and pass it to
+     * getStrings(), where the real scrambling begins.
+     */
+    String userString = userInput.next();
+    for (int loop = 0;
+    loop < characterCount;
+    loop++
+    ) {
+        characters[loop] = userString.charAt(loop);
+    }
     // YOUR CODES TO CALL THE getString() methods and
     // display the results.
 }
