de0f7bbed5adf752cd15c5144e31a9bd16fda19e
Jonathan Smalls <jon@smalls.rocks>
2019-10-13 09:18:06 -0400

    documentation: starting largeW{} scaffolding with problem description

---
+++ 6/largeW.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/**
+ * Description:
+ * Instead of dividing into half at each step, you are now supposed to divide
+ * recursively the initial array into thirds, sort each third, and then combine
+ * using a 3-way merge. Please implement a MergeSort3Way class so that arrays
+ * can besorted by using Merge Sort 3-Way.Please read the numbers from the
+ * attached largeW.txtfile into an array, apply the 3-way sorting, and output
+ * the sorted results into a file largeWResults.txt
+ */
+public class largeW
+{
+public static void main(String[] args)
+{
+}
+}
