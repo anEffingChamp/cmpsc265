b709aadce66b74d7b2fd8ba56a82bf11cc4e0481
Jonathan Smalls <jon@smalls.rocks>
2019-11-03 12:29:22 -0500

    bug: failed to insert nodes after root
    I see the problem. My switch() should fix that, so lets keep rocking.

--- 8/TwoDTree.java
+++ 8/TwoDTree.java
@@ -63,7 +63,7 @@ public void insert(Point2D input)
     if (null == this.root) {
         this.root = nextNode;
     }
-    this._insertNode(this.root, nextNode, 0);
+    this._insertNode(this.root, nextNode, 1);
 }
 /**
  * _insertNode() adds a node according to two dimensional, binary search tree
@@ -72,29 +72,26 @@ public void insert(Point2D input)
  */
 private void _insertNode(Node currentNode, Node input, int currentLevel)
 {
-    Node nextNode = null;
+    Node nextNode = currentNode.left;
+    int direction = 0;
     /*
      * We insert according to the X coordinate on odd numbered levels. We decide
      * on the next motion through the binary tree here, although we execute it
      * later in the method.
      */
     if (1 == currentLevel % 2) {
-        if (currentNode.p.x > input.p.x) {
-            nextNode = currentNode.left;
-        }
         if (currentNode.p.x <= input.p.x) {
-            nextNode = currentNode.right;
+            nextNode  = currentNode.right;
+            direction = 1;
         }
     }
     /*
      * We insert by the Y coordinate on even numbered levels.
      */
     if (0 == currentLevel % 2) {
-        if (currentNode.p.y > input.p.y) {
-            nextNode = currentNode.left;
-        }
         if (currentNode.p.y <= input.p.y) {
-            nextNode = currentNode.right;
+            nextNode  = currentNode.right;
+            direction = 1;
         }
     }
     /*
@@ -103,10 +100,16 @@ private void _insertNode(Node currentNode, Node input, int currentLevel)
      * tree.
      */
     if (null == nextNode) {
-        nextNode = input;
+        switch (direction) {
+        case 1:
+            currentNode.right = input;
+            break;
+        default:
+            currentNode.left = input;
+        }
         return;
     }
-    this._insertNode(nextNode, input, currentLevel++);
+    this._insertNode(nextNode, input, currentLevel);
 }
 public boolean search(Point2D p){
     // YOUR CODES
@@ -162,19 +165,16 @@ public static void main(String[] args) throws Exception {
          * We read the file according to the instructions at Java67.
          * https://www.java67.com/2012/11/how-to-read-file-in-java-using-scanner-example.html
          */
-        Scanner fileInput     = new Scanner(new File("input10K.txt"));
-        double fileCoordinate = 0;
-        int countCoordinates  = 0;
-        while (true == fileInput.hasNext()) {
-            if (0 == countCoordinates % 2) {
-                fileTree.insert(
-                    new Point2D(fileCoordinate, fileInput.nextDouble())
-                );
-            }
-            if (1 == countCoordinates % 2) {
-                fileCoordinate = fileInput.nextDouble();
-            }
-            countCoordinates++;
+        Scanner fileInput = new Scanner(new File("input10K.txt"));
+        while (true == fileInput.hasNextLine()) {
+            String nextLine     = fileInput.nextLine();
+            String[] nextValues = nextLine.split(" ");
+            fileTree.insert(
+                new Point2D(
+                    Double.parseDouble(nextValues[0]),
+                    Double.parseDouble(nextValues[1])
+                )
+            );
         }
     } catch(FileNotFoundException exception) {
         System.out.println("The file was not found.");
