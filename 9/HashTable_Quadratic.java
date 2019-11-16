4258998f51f6aebd931ce156d580b0d09a2a201f
Jonathan Smalls <jon@smalls.rocks>
2019-11-15 22:29:36 -0500

    feature: converting string to hashed integer in HashTable{}

--- 9/HashTable_Quadratic.java
+++ 9/HashTable_Quadratic.java
@@ -1,10 +1,27 @@
 import java.io.*;
 import java.util.*;
-
+/**
+ * Description:Please finish definition of the HashTable_Quadratic class, so
+ * that a hash table for strings can be built, and using quadratic probingrather
+ * than linear-probing to solve collision.To compute for the hash value of a
+ * given string, for example, CLRS, you can use the following approach:1)First,
+ * We need to first convert the string to be an integer. One way to do this is
+ * to use the ASCII value of every character.
+ * - ASCII values: C=67, L=76, R=82,
+ * S=83.
+ * - There are 128 basic ASCII values.
+ *   - So, CLRS =67*1283+76 *1282+ 82*1281+
+ * 83*1280= 141,764,947.
+ * 2)Secondly, transform the integer into an index in the array representing
+ * the hash table.  Suppose the size of the hash table is M,
+ * - The hash value of 141,764,947 would be:
+ * - 141,764,947 % MMake sure you areusing Quadratic probing
+ * to solve collision
+ */
 public class HashTable_Quadratic {
  private String[] hashArray; // array holds hash table
  private int arraySize;
- 
+
  // -------------------------------------------------------------
  public HashTable_Quadratic(int size)       // constructor
    {
@@ -29,19 +46,32 @@ public class HashTable_Quadratic {
   // YOUR CODES
   return  -1; // For compilation. You need to change it.
  }
-
- // -------------------------------------------------------------
- public void insert(String item) // insert a DataItem
- // (assumes table not full)
- {  // YOUR CODES
-  
- } // end insert()
+/**
+ * insert() adds a value to the HashTable{} with quadratic problem to resolve
+ * any collisions.
+ */
+public void insert(String input) // insert a DataItem
+// (assumes table not full)
+{
+    /*
+     * We convert the string to an integer first.
+     */
+    int stringInteger = 0;
+    for (int loop = 0;
+    loop < input.length();
+    loop++
+    ) {
+        stringInteger +=
+            (int) input.charAt(loop) * Math.pow(26, input.length() - loop - 1);
+    }
+    System.out.println(stringInteger);
+}
   // -------------------------------------------------------------
 
  public String delete(String key) // delete a DataItem
  {  // YOUR CODES
   return null; // For compilation. You need to change it.
-  
+
  } // end delete()
   // -------------------------------------------------------------
 
@@ -49,18 +79,19 @@ public class HashTable_Quadratic {
  {  // YOUR CODES
   return null; // For compilation. You need to change it.
  }
- 
+
  // -------------------------------------------------------------
  // end class HashTable
- 
-   public static void main(String[] args) throws IOException {
+
+public static void main(String[] args) throws IOException
+{
     int aKey, size, n, keysPerCell;
-          
+
     //get the size of the HashTable
     Scanner console = new Scanner(System.in);
     System.out.print("Enter size of hash table: ");
     size = console.nextInt();
-    
+
           //make the table
     HashTable_Quadratic table = new HashTable_Quadratic(size);
     table.insert("apple");
@@ -73,8 +104,8 @@ public class HashTable_Quadratic {
     table.insert("horse");
     table.insert("ice");
     table.insert("jungle");
-    
+
     // display the table
     table.displayTable();
-   }
+}
 }
